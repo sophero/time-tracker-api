a76bf5358f37a203a73d0aaba751c8b77bbe0afd
Sophia Di <backwardsxmarathon@gmail.com>
2018-01-23 10:07:01 -0500

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+require('./config/config');
+
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
+
+var { mongoose } = require('./db/mongoose');
+var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
+
+var app = express();
+const port = process.env.PORT;
+
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
+
+module.exports = { app };
