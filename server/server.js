be185428cd6e6a36df8ab7507c798d8f5fb6ec42
Sophia Di <backwardsxmarathon@gmail.com>
2018-01-23 10:51:33 -0500

    Add POST /users route and token method.

--- server/server.js
+++ server/server.js
@@ -7,12 +7,28 @@ const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { User } = require('./models/user');
-var { authenticate } = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
+
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+  user
+    .save()
+    .then(() => {
+      return user.generateAuthToken();
+    })
+    .then(token => {
+      // res.header allows you to set a key-value pair, whereas req.header returns the value for your key.
+      res.header('x-auth', token).send(user);
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
